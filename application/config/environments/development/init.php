a04eee3e297e4e375f75cd5977ed2ed02f9acd55
Lorenzo Pisani <zeelot3k@gmail.com>
2012-08-25 00:23:53 -0700

    fix url

--- application/config/environments/development/init.php
+++ application/config/environments/development/init.php
@@ -2,7 +2,7 @@
 
 return array(
 	// @TODO: change this to be project-specific
-	'base_url'    => '/',
+	'base_url'    => 'http://33.33.33.10/',
 	'index_file'  => '',
 	'charset'     => 'utf-8',
 	'cache_dir'   => APPPATH.'cache',
