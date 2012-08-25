0a23a960d8afa0adf9193b5198498b5cb171c580
Lorenzo Pisani <zeelot3k@gmail.com>
2012-08-25 00:23:42 -0700

    add oauth config for github API

---
+++ application/config/oauth.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+	'github' => array(
+		'client_id'    => '1fc32871b72145b719eb',
+		'secret'       => '3612034e9150e51faa0dcda93cdb71865d00b923',
+		'oauth_dialog' => 'https://github.com/login/oauth/authorize',
+		'oauth_token'  => 'https://github.com/login/oauth/access_token',
+		'api_url'      => 'https://api.github.com/',
+	),
+);
