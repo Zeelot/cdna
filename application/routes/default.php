c2205fe330b3e46524aeeae6468e7dbbc7212188
Lorenzo Pisani <zeelot3k@gmail.com>
2012-08-25 00:23:13 -0700

    home and log-in routes

--- application/routes/default.php
+++ application/routes/default.php
@@ -4,8 +4,14 @@
  * Set the routes. Each route must have a minimum of a name, a URI and a set of
  * defaults for the URI.
  */
-Route::set('default', '(<controller>(/<action>(/<id>)))')
+Route::set('home', '')
 	->defaults(array(
 		'controller' => 'main',
 		'action'     => 'index',
+	));
+
+Route::set('log-in', 'login')
+	->defaults(array(
+		'controller' => 'auth',
+		'action'     => 'login',
 	));
